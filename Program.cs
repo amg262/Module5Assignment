3ac6534dc30e2b7bb998d53e6386f9e3ede5553e
Andrew <andrewmgunn26@gmail.com>
2021-09-19 17:18:37 -0500

    building out PreStock method and VendingMachine

--- Program.cs
+++ Program.cs
@@ -1,12 +1,54 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 
 namespace Module5Assignment
 {
     class Program
     {
+
+        public Stack<string> PreStock(Stack<string> stack)
+        {
+
+            if (stack.Count > 0)
+            {
+                return stack;
+            }
+            
+            var items = new List<string>()
+            {
+                "Mtn Dew",
+                "Human Finger",
+                "Cheesecake"
+            };
+
+            var localStack = new Stack<string>();
+            
+            foreach (var item in items)
+            {
+                localStack.Push(item);
+            }
+            
+            return localStack;
+        }
+
+        public void VendingMachine()
+        {
+
+            var stack = new Stack<string>();
+
+            Console.WriteLine("VENDING MACHINE");
+            this.PreStock(stack);
+            
+            
+
+
+        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Program p = new Program();
+            p.VendingMachine();
         }
     }
 }
